4c1f1eace84a39f47f78db1f98bbf04a398854f4
Yash Bahuguna <yashbahuguna918@gmail.com>
2025-08-12 02:32:36 +0530

    first commit

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -1,9 +1,17 @@
-import Navbar from "../components/Navbar";
+import {Link,Navlink} from 'react-router-dom';
 
 export default function Home() {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <>
+    <nav className="flex items-center justify-between p-5">
+      <button className="text-7xl py-1.5 px-5">SKILLSWAP</button>
+      <div className="flex gap-20">
+        <button className="text-4xl px-10">home</button>
+        <button className="text-4xl px-10">templates</button>
+        <button className="text-4xl px-10">about</button>
+        <button className="text-4xl px-10">pricing</button>
+      </div>
+    </nav>
+    </>
   );
 }
