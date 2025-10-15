a2e63f314d661b2eaa95909ca5c9723ba939246b
yash bahuguna <yashbahuguna918@gmail.com>
2025-10-15 23:38:34 +0530

    added

---
+++ backend/services/videoCallService.js
@@ -0,0 +1,17 @@
+import { Server } from "socket.io";
+
+export function initSocket(server) {
+  const io = new Server(server, { cors: { origin: "*" } });
+
+  io.on("connection", (socket) => {
+    socket.on("join-room", (roomId, userId) => {
+      socket.join(roomId);
+      socket.to(roomId).emit("user-connected", userId);
+      socket.on("disconnect", () => socket.to(roomId).emit("user-disconnected", userId));
+    });
+
+    socket.on("signal", (data) => {
+      io.to(data.to).emit("signal", { from: data.from, signal: data.signal });
+    });
+  });
+}
