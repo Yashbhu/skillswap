f448781fe064917506ae2435b8ba453e6d9ec076
yash bahuguna <yashbahuguna918@gmail.com>
2025-10-15 23:48:08 +0530

    done this shit too

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import app from "./app.js";
+import dotenv from "dotenv";
+import http from "http";
+import { setupSocket } from "./services/socketService.js";
+import { initSocket } from "./services/videoCallService.js";
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
